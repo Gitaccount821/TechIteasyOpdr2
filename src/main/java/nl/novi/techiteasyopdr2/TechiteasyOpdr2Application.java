27d0197426c4a76618fb6d02138cfef24dc81167
Gitaccount821 <nealvanzomeren@gmail.com>
2024-12-01 17:07:18 +0100

    first commit

---
+++ src/main/java/nl/novi/techiteasyopdr2/TechiteasyOpdr2Application.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasyopdr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechiteasyOpdr2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechiteasyOpdr2Application.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/techiteasyopdr2/TechiteasyOpdr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasyopdr2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechiteasyOpdr2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
